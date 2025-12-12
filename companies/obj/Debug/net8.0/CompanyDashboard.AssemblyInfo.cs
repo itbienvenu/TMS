9f833544f73814f04436100329f6e1ddd83d6247
itbienvenu <bienvenugashema@gmail.com>
2025-12-12 21:09:21 +0200

    Setting the ci-cd pipeline

--- companies/obj/Debug/net8.0/CompanyDashboard.AssemblyInfo.cs
+++ companies/obj/Debug/net8.0/CompanyDashboard.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CompanyDashboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f19e97f906c62d326c1b5427007f0e4f255934d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f987761c352bf1c92fffb932bf49aa93aec9f837")]
 [assembly: System.Reflection.AssemblyProductAttribute("CompanyDashboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CompanyDashboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
